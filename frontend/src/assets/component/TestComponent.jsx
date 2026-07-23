83c932eac10e510adea2e4076e0db6d7b00da87f
Lse-Yeong <ocdee39@gmail.com>
2026-07-23 12:56:26 +0900

    feat: 초기 디렉토리 구조 구축

---
+++ frontend/src/assets/component/TestComponent.jsx
@@ -0,0 +1,9 @@
+function TestComponent() {
+    return (
+        <div>
+            Test Component
+        </div>
+    )
+}
+
+export default TestComponent;
---
+++ frontend/src/page/TestPage.jsx
@@ -0,0 +1,11 @@
+import TestComponent from "../assets/component/TestComponent";
+
+function TestPage() {
+    return (
+        <div>
+            <TestComponent></TestComponent>
+        </div>
+    )
+}
+
+export default TestPage;
